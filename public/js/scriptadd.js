bd2dd57b428ec08f947af0f2c5b4b95c23ffd36a
agusslam <agus.slamethermawa@gmail.com>
2021-10-26 23:02:00 +0700

    update condition

--- public/js/scriptadd.js
+++ public/js/scriptadd.js
@@ -48,31 +48,61 @@ function delete_cookie( name, path, domain ) {
         valName = document.querySelector('#inputName').value
         valStock = document.querySelector('#inputStock').value
         valPrice = document.querySelector('#inputPrice').value        
-        valImg = btnImg.files[0].name
+        
+        if(btnImg.files[0] == null || btnImg.files[0] == undefined){ 
+            document.querySelector('#alert-add-danger2').classList.remove('hide')
+            document.querySelector('#alert-add-danger2').classList.add('show')
+         }else { 
+             valImg = btnImg.files[0].name 
+             const postAdd = await fetch('/product-api-add', {
+                method: 'POST',
+                headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${dataCookie}`
+                        },
+                body: JSON.stringify({
+                        nameprod: valName, 
+                        stock: valStock,
+                        price: valPrice,
+                        img: valImg
+                    })
+                }).then(response => {return response.json()}).catch(err => {return err})
+                    // console.log(addNew)
+                    if(postAdd.status == 200) {
+                        document.querySelector('#alert-add-success').classList.remove('hide')
+                        document.querySelector('#alert-add-success').classList.add('show')
+                    }else {
+                        document.querySelector('#alert-add-danger').classList.remove('hide')
+                        document.querySelector('#alert-add-danger').classList.add('show')
+                    }
+        } 
 
-        const postAdd = await fetch('/product-api-add', {
-        method: 'POST',
-        headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${dataCookie}`
-                },
-        body: JSON.stringify({
-                nameprod: valName, 
-                stock: valStock,
-                price: valPrice,
-                img: valImg
-            })
-        }).then(response => {return response.json()}).catch(err => {return err})
-            // console.log(addNew)
-            if(postAdd.status == 200) {
-                document.querySelector('#alert-add-success').classList.remove('hide')
-                document.querySelector('#alert-add-success').classList.add('show')
-            }else {
-                document.querySelector('#alert-add-danger').classList.remove('hide')
-                document.querySelector('#alert-add-danger').classList.add('show')
+        // btn logout act
+    btnLogout = document.querySelector('#btn-signout')
+    btnLogout.addEventListener('click', async () => {
+        try {
+            let dataCookie = JSON.parse(getCookie('_SpaidRE'))
+            const logOut = await fetch('/user-api-logout', {
+                method: 'POST',
+                headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${dataCookie}`
+                        }
+                }).then(response => {return response.json()}).catch(err => {return "error"})
+            if(logOut.auth == false) {
+                delete_cookie('_SpaidRE')
+                location.reload()
             }
-        })        
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    }).catch(err => {console.log(err)})
 
     btnBac = document.querySelector('#btn-back')
     btnBac.addEventListener('click', () => { window.location.href = '/product-home' })
+
+    
